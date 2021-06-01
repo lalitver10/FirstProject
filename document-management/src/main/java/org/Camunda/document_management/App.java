850fc58bc1e31dd42157f58af3407a9fa3907d7f
Lalit Verma <lalitver10@gmail.com>
2021-06-01 14:28:18 +0530

    add demo method

--- document-management/src/main/java/org/Camunda/document_management/App.java
+++ document-management/src/main/java/org/Camunda/document_management/App.java
@@ -6,5 +6,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println("Hii Lalit Verma");
+        callDemo();
     }
+
+	private static void callDemo() {
+		
+	}
 }
